f2bb119045cadb53ff570f753dcc14af0dc91ff1
share121 <2854631158@qq.com>
2025-04-03 22:15:51 +0800

    fix: 使调度线程单线程调度，修复了计算重复、漏算的 bug

--- src/spawn.rs
+++ src/spawn.rs
@@ -27,7 +27,7 @@ where
     thread::spawn(move || {
         thread::scope(|s| {
             let mut workers = Vec::with_capacity(task_group.len());
-            let (tx_progress, rx_progress) = crossbeam_channel::unbounded();
+            let (tx_progress, rx_progress) = crossbeam_channel::bounded(0);
             for (id, tasks) in task_group.into_iter().enumerate() {
                 let (tx_task, rx_task) = crossbeam_channel::unbounded();
                 let tx_progress = tx_progress.clone();
@@ -82,59 +82,59 @@ where
     })
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use crate::task::Task;
-//     use std::collections::{HashMap, hash_map::Entry};
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::task::Task;
+    use std::collections::{HashMap, hash_map::Entry};
 
-//     fn fib(n: u128) -> u128 {
-//         match n {
-//             0 => 0,
-//             1 => 1,
-//             _ => fib(n - 1) + fib(n - 2),
-//         }
-//     }
+    fn fib(n: u128) -> u128 {
+        match n {
+            0 => 0,
+            1 => 1,
+            _ => fib(n - 1) + fib(n - 2),
+        }
+    }
 
-//     #[test]
-//     fn test_spawn() {
-//         let tasks = vec![Task {
-//             start: 0u128,
-//             end: 44u128,
-//         }];
-//         let task_group = tasks.split_task(8);
-//         let (tx, rx) = crossbeam_channel::unbounded();
-//         let handle = spawn(task_group, move |rx_task, progress| {
-//             'task: for tasks in &rx_task {
-//                 if tasks.is_empty() {
-//                     break;
-//                 }
-//                 for task in tasks {
-//                     for i in task.start..task.end {
-//                         if !rx_task.is_empty() {
-//                             continue 'task;
-//                         }
-//                         println!("开始计算 {}", i);
-//                         progress(1);
-//                         let res = fib(i);
-//                         tx.send((i, res)).unwrap();
-//                     }
-//                 }
-//             }
-//         });
-//         let mut data = HashMap::new();
-//         for (i, res) in rx {
-//             match data.entry(i) {
-//                 Entry::Occupied(_) => panic!("数字 {i}，值为 {res} 重复计算"),
-//                 Entry::Vacant(entry) => {
-//                     entry.insert(res);
-//                 }
-//             }
-//         }
-//         handle.join().unwrap();
-//         dbg!(&data);
-//         for i in tasks[0].start..tasks.last().unwrap().end {
-//             assert_eq!((i, data.get(&i)), (i, Some(&fib(i))));
-//         }
-//     }
-// }
+    #[test]
+    fn test_spawn() {
+        let tasks = vec![Task {
+            start: 0u128,
+            end: 44u128,
+        }];
+        let task_group = tasks.split_task(8);
+        let (tx, rx) = crossbeam_channel::unbounded();
+        let handle = spawn(task_group, move |rx_task, progress| {
+            'task: for tasks in &rx_task {
+                if tasks.is_empty() {
+                    break;
+                }
+                for task in tasks {
+                    for i in task.start..task.end {
+                        if !rx_task.is_empty() {
+                            continue 'task;
+                        }
+                        println!("开始计算 {}", i);
+                        progress(1);
+                        let res = fib(i);
+                        tx.send((i, res)).unwrap();
+                    }
+                }
+            }
+        });
+        let mut data = HashMap::new();
+        for (i, res) in rx {
+            match data.entry(i) {
+                Entry::Occupied(_) => panic!("数字 {i}，值为 {res} 重复计算"),
+                Entry::Vacant(entry) => {
+                    entry.insert(res);
+                }
+            }
+        }
+        handle.join().unwrap();
+        dbg!(&data);
+        for i in tasks[0].start..tasks.last().unwrap().end {
+            assert_eq!((i, data.get(&i)), (i, Some(&fib(i))));
+        }
+    }
+}
