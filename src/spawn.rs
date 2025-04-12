8840e65338f491f92524c86e091e1c35f6056863
share121 <2854631158@qq.com>
2025-04-12 23:14:47 +0800

    test: 完善测试

--- src/spawn.rs
+++ src/spawn.rs
@@ -174,4 +174,121 @@ mod tests {
             assert_eq!((index, data.get(&index)), (index, Some(&fib_fast(index))));
         }
     }
+
+    #[test]
+    fn test_spwan3() {
+        let tasks: Arc<TaskList> = Arc::new(vec![0..48].into());
+        let (tx, rx) = mpsc::channel();
+        let tasks_clone = tasks.clone();
+        let handles = tasks.clone().spawn(
+            8,
+            |closure| thread::spawn(move || closure()),
+            move |_, task, get_task| {
+                loop {
+                    while task.start() < task.end() {
+                        let i = tasks_clone.get(task.start());
+                        task.fetch_add_start(3);
+                        tx.send((i, fib(i))).unwrap();
+                        if i + 2 < task.end() {
+                            tx.send((i + 1, fib(i + 1))).unwrap();
+                            tx.send((i + 2, fib(i + 2))).unwrap();
+                        } else if i + 1 < task.end() {
+                            tx.send((i + 1, fib(i + 1))).unwrap();
+                            task.fetch_sub_start(1);
+                        } else {
+                            task.fetch_sub_start(2);
+                        }
+                    }
+                    if !get_task() {
+                        break;
+                    }
+                }
+            },
+        );
+        // 汇总任务结果
+        let mut data = HashMap::new();
+        for (i, res) in rx {
+            // 如果重复计算就报错
+            match data.entry(i) {
+                Entry::Occupied(_) => {
+                    panic!("数字 {i}，值为 {res} 重复计算")
+                }
+                Entry::Vacant(entry) => {
+                    entry.insert(res);
+                }
+            }
+            data.insert(i, res);
+        }
+        // 等待任务结束
+        for handle in handles {
+            handle.join().unwrap();
+        }
+        // 验证结果
+        dbg!(&data);
+        for i in 0..tasks.len {
+            let index = tasks.get(i);
+            assert_eq!((index, data.get(&index)), (index, Some(&fib_fast(index))));
+        }
+    }
+
+    #[test]
+    fn test_spwan4() {
+        let tasks: Arc<TaskList> = Arc::new(vec![0..48].into());
+        let (tx, rx) = mpsc::channel();
+        let tasks_clone = tasks.clone();
+        let handles = tasks.clone().spawn(
+            8,
+            |closure| thread::spawn(move || closure()),
+            move |_, task, get_task| {
+                loop {
+                    while task.start() < task.end() {
+                        let i = tasks_clone.get(task.start());
+                        task.fetch_add_start(4);
+                        tx.send((i, fib(i))).unwrap();
+                        if i + 3 < task.end() {
+                            tx.send((i + 1, fib(i + 1))).unwrap();
+                            tx.send((i + 2, fib(i + 2))).unwrap();
+                            tx.send((i + 3, fib(i + 3))).unwrap();
+                        } else if i + 2 < task.end() {
+                            tx.send((i + 1, fib(i + 1))).unwrap();
+                            tx.send((i + 2, fib(i + 2))).unwrap();
+                            task.fetch_sub_start(1);
+                        } else if i + 1 < task.end() {
+                            tx.send((i + 1, fib(i + 1))).unwrap();
+                            task.fetch_sub_start(2);
+                        } else {
+                            task.fetch_sub_start(3);
+                        }
+                    }
+                    if !get_task() {
+                        break;
+                    }
+                }
+            },
+        );
+        // 汇总任务结果
+        let mut data = HashMap::new();
+        for (i, res) in rx {
+            // 如果重复计算就报错
+            match data.entry(i) {
+                Entry::Occupied(_) => {
+                    panic!("数字 {i}，值为 {res} 重复计算")
+                }
+                Entry::Vacant(entry) => {
+                    entry.insert(res);
+                }
+            }
+            data.insert(i, res);
+        }
+        // 等待任务结束
+        for handle in handles {
+            handle.join().unwrap();
+        }
+        // 验证结果
+        dbg!(&data);
+        for i in 0..tasks.len {
+            let index = tasks.get(i);
+            assert_eq!((index, data.get(&index)), (index, Some(&fib_fast(index))));
+        }
+    }
 }
